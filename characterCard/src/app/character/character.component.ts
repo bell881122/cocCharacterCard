83ea5466798f8c80f1d79edbe53708eb381ef4ac
sandy <sandy@atinfo.com.tw>
2019-10-15 18:19:28 +0800

    update mutiCharactorCard --done

--- characterCard/src/app/character/character.component.ts
+++ characterCard/src/app/character/character.component.ts
@@ -1,6 +1,4 @@
-import { Component, Input, ViewChild, AfterViewInit } from '@angular/core';
-import { IfStmt } from '@angular/compiler';
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'character',
@@ -20,52 +18,74 @@ export class CharacterComponent {
 
     chCardBox = [];
 
-    charactorNum = 1;
-    CHcard = 'CH' + this.charactorNum;
+    CHcard = 'CH1';
 
     ngOnInit() {
         if (localStorage.getItem(this.CHcard) == null) {
             let data = { "Charactor": this.CHcard };
             localStorage.setItem(this.CHcard, JSON.stringify(data));
         }
-        console.log(JSON.parse(localStorage.getItem(this.CHcard)));
+        // console.log(JSON.parse(localStorage.getItem(this.CHcard)));
 
         for (let i = 0; i < localStorage.length; i++) {
             let data = localStorage.key(i);
             this.chCardBox.push(data);
             // this.chCardBox[data] = JSON.parse(localStorage.getItem(data));
-            console.log(this.chCardBox);
-
         }
-
+        this.lastCard = this.CHcard;
+        // console.log(this.chCardBox);
+        // localStorage.removeItem("aaa");
     }
 
     checkoutCHcard;
-    chooseCard(c){
-        this.CHcard=c;
+    chooseCard(c) {
+        this.CHcard = c;
         console.log(c);
     }
 
-    addNewCHcardData="";
-    addNewCHcard() {
-        if (this.addNewCHcardData == ""){
+    // 以新名稱儲存檔案
+    addNew = false;
+    addNewTrue() {
+        if (this.addNewCHcardData == "") {
             alert("請輸入角色卡名稱");
-        }else{
-            console.log(this.addNewCHcardData);
-            // this.CHcard = this.addNewCHcardData;
+            return;
+        } else {
+            this.addNew = true;
         }
-
     }
 
+    addNewCHcardData = "";
+    lastCard;
+    addNewCHcard() {
+        if (this.addNewCHcardData == "") {
+            // alert("請輸入角色卡名稱");
+            return;
+        } else {
+            this.lastCard = this.CHcard;
+            this.CHcard = this.addNewCHcardData;
+        }
+        // console.log(this.chCardBox);
+    }
 
+    addNewFalse() {
+        this.addNew = false;
+        this.addNewCHcardData = "";
+    }
+
+    DontSave(){
+        this.CHcard = this.lastCard;
+    }    
+    // console.log(this.addNewCHcardData);
 
     saveAll() {
         if (localStorage.getItem(this.CHcard) == null) {
             let data = { "Charactor": this.CHcard };
             localStorage.setItem(this.CHcard, JSON.stringify(data));
+            this.chCardBox.push(data["Charactor"]);
         }
-
+        this.addNewCHcardData = "";
         // alert('所有資料已儲存');
+        // console.log(this.chCardBox);
     }
 
 }
